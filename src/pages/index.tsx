8f4a1db0cb4d83076d92270dc4a191fd1a39096a
marcogandra <marco.gandra@outlook.com>
2021-08-03 14:44:37 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,33 +39,9 @@ const Home: React.FC = () => {
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue" rel="stylesheet" />
-
       </Head>
 
-      <main >
-        <div className="sarjeta">
-          <div className="container-fluid">
-            <div className="row" >
-              <div className="col-md-1" ></div>
 
-                <div className="col-md-10 principal">
-                  < Cabecalho/>
-                  <Social/>
-                  <Carrocel/>
-                  <Caixa1 />
-                  <Caixa2 />
-                  <Caixa3 />
-                  <Fundadores/>
-                  <Contato/>
-                  <Rodape/>
-                </div>
-
-              <div className="col-md-1" ></div>
-            </div>
-          </div>
-        </div>
-
-      </main>
 
     </div>
   )
